f9b294fd10230d71f59ff9447975b9fc1038419f
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-01-20 14:19:29 +0530

    initial commit

--- src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -3,10 +3,7 @@ package com.example.layeredarchitecture.dao;
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.CustomerDTO;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CustomerDAOImpl {
@@ -24,4 +21,24 @@ public class CustomerDAOImpl {
         }
         return customers;
     }
+
+    public void saveCustomer(String id, String name, String address) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, id);
+        pstm.setString(2, name);
+        pstm.setString(3, address);
+        pstm.executeUpdate();
+    }
+
+    public void updateCustomer(String id, String name, String address) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+        pstm.setString(1, name);
+        pstm.setString(2, address);
+        pstm.setString(3, id);
+        pstm.executeUpdate();
+    }
+
+    public void exitCustomer(String id) throws SQLException, ClassNotFoundException {}
 }
